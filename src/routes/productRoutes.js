b037230ea958bb22c6577ec9075700c929d5dd99
shahal <shahal.c077@gmail.com>
2026-02-04 19:34:58 +0530

    admin dashbord error1  fix

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,1 +1,12 @@
-scp -r public ubuntu@ip-172-31-5-71:/var/www/html/
+const express = require("express");
+const router = express.Router();
+const upload = require("../middlewares/uploadMiddleware");
+const {createProduct,getProducts,deleteProduct,updateProduct} = require("../controllers/AdminProductController");
+const auth = require("../middlewares/authMiddleware");
+
+router.post("/", upload.array("images", 3),auth,createProduct);
+router.put("/:id", upload.array("images", 3),auth,updateProduct); 
+router.get("/",getProducts);
+router.delete("/:id",auth,deleteProduct);
+
+module.exports = router;
